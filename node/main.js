e9fc26ab45fa9c5819ab24ebf1cc6b6c9decf16f
Kensa <kensadevs@gmail.com>
2020-12-20 19:01:20 +0100

    brrrrr

--- node/main.js
+++ node/main.js
@@ -1,19 +1,17 @@
 const livebox = require("./livebox");
 
-
+const target = 'D8:C4:6A:B5:EF:FD';
 
 async function main(){
 
-    let res = await livebox.login('192.168.1.1',process.env.livebox_user,process.livebox_password);
-    let token = res.token;
-    let cookie = res.cookie;
-    //console.log(`token : ${token}`);
+    let loginRes = await livebox.login('192.168.1.1',process.env.livebox_user,process.livebox_password);
+    
     let options = {
         host:'192.168.1.1',
-        token:token,
-        cookie:cookie,
+        token:loginRes.token,
+        cookie:loginRes.cookie,
         info:{
-            mac:'D8:C4:6A:B5:EF:FD'
+            mac:target
         }
     }
 
